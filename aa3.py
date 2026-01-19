79fa170c1e6f7491d43448a92f789f3501829ad1
pushkalmehrotra <pushkalmehrotra774@gmail.com>
2026-01-19 13:08:33 +0530

    initial.commit

---
+++ aa3.py
@@ -0,0 +1,11 @@
+setc1{"green","blue"}
+setc2= {"blue","yellow"}
+print("Original sets: ")
+print(setc1)
+print(setc2)
+setc = setc1.union(setc2)
+print("\nUnion of above sets: ")
+print(setc)
+print("\nIntersection of two said sets:")
+setz = setc1.intersection(setc2)
+print(setz)
